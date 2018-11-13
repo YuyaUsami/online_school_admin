85976ed6e8120e9fdccfde75c700b08d97c7cc15
YuyaUsami <yu0524.engineer@gmail.com>
2018-11-14 01:14:37 +0900

    言語指定のtypoを修正

--- online_school_admin/settings.py
+++ online_school_admin/settings.py
@@ -104,7 +104,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
 
-LANGUAGE_CODE = 'js'
+LANGUAGE_CODE = 'ja'
 
 TIME_ZONE = 'Asia/Tokyo'
 
